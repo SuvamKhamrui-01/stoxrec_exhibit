c8d4d53ab927489341d40d5adddd2882f799c643
SuvamKhamrui-01 <142648727+SuvamKhamrui-01@users.noreply.github.com>
2023-08-31 17:55:55 +0530

    Create frontend1.py

---
+++ frontend1.py
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        body {
+          /* background-image:url('logo1.jpg'); */
+          /* background-repeat: no-repeat; */
+          background:url('logo1.jpg') ;
+          background-size: 100% ;
+        }
+        .btn{
+            font-family: 'Georgia';
+            margin:450px 700px;
+            background-color: rgb(122, 123, 123);
+            /* color: rgb(59, 61, 59); */
+            padding: 4px 14px;
+            border: 2px solid grey;
+            border-radius: 10px;
+            font-size: 20px;
+            cursor: pointer;
+            width: 11.5%;
+            box-shadow: 4px 4px;
+            /* text-decoration:none; */
+            /* box-shadow: 0 0 0 1px #26334a inset,0 0 0 2px rgba(255, 255, 255, 0.15) inset,0 4px 0 0      #414142,0 4px 0 1px rgba(0, 0, 0, 0.4),0 4px 4px 1px rgba(0, 0, 0, 0.5); */
+           }
+           .btn a{
+            color: black;
+            text-decoration:none;
+
+           }
+           .btn:hover {
+            background-color: rgb(226, 223, 223);
+            color: rgb(80, 74, 74);
+            text-align: center;
+        }
+        </style>
+</head>
+<body>
+    <div class="f1">
+        <button class="btn"><a href="signin.html">JOIN US>></a></button>
+    </div>
+    
+</body>
+</html>
